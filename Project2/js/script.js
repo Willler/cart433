4ef484572d3344167a13f7b2cb21665d87711177
William L'Eriger <espermentalout@gmail.com>
2020-10-09 00:23:40 -0400

    Working Graph

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+function startMe() {
+
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'polarArea',
+
+    // The data for our dataset
+    data: {
+        labels: ['Gun', 'Bare Fists', 'Wire', 'Talk'],
+        datasets: [{
+            backgroundColor: ['rgb(255, 99, 132)','rgb(0, 99, 132)','rgb(255, 99, 0)', 'black'],
+            borderWidth: '0',
+            data: [2,8,6,3]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: {
+        display: false
+        }
+      }
+    
+});
+}
