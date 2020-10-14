0c440486b97170bc4423d68f22ddbfedf740dfe5
William L'Eriger <espermentalout@gmail.com>
2020-10-14 18:18:36 -0400

    Lotsa progress done, still looks terrible

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -1,49 +1,61 @@
 "use strict";
 
+// for section 1
 var ctx = document.getElementById('myChart').getContext('2d');
 
+//for section 2
+var canvas;
+let activationButton;
 
-function startMe() {
+let bulletX = 450;
+let bulletVX = 1;
+
+let shotBullet = false;
+
+let killSize = 50;
+let killX;
+
+
+// first graph
+function killedBy() {
 
   var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'polarArea',
+    type: 'doughnut',
 
     // The data for our dataset
     data: {
-        labels: ['Gun', 'Bare Fists', 'Wire', 'Talk'],
+        labels: ['John', 'Marcus', 'Viggo', 'Winston', 'Ms. Pierce', 'Iosef'],
         datasets: [{
-            backgroundColor: ['rgb(255, 99, 132)','rgb(0, 99, 132)','rgb(255, 99, 0)', 'black'],
+            backgroundColor: ['black', 'white', 'green', 'blue', 'gold', 'red', 'cyan',],
             borderWidth: '0',
-            data: [2,8,6,3]
+            data: [77, 3, 1, 1, 1, 1]
         }]
     },
 
     // Configuration options go here
     options: {
       legend: {
-        // display: false
+        position: 'bottom'
         }
       }
 
 });
 }
 
-
-
-
+//second graph
 function weaponStart() {
 
   var chart = new Chart(ctx, {
       // The type of chart we want to create
-      type: 'polarArea',
+      type: 'doughnut',
 
       // The data for our dataset
       data: {
           labels: ['Pistol', 'Assault Rifle', 'Sniper', 'Shotgun', 'Bare Hands', 'Knife', 'Vehicle', 'Explosives'],
           datasets: [{
               backgroundColor: ['black', 'white', 'green', 'blue', 'gold', 'red', 'cyan', 'magenta'],
-              borderWidth: '1',
+              borderWidth: '0',
               data: [41, 14, 4, 2, 4, 4, 5, 3]
           }]
       },
@@ -64,4 +76,53 @@ function weaponStart() {
         }
 
   });
+
+}
+
+function setup() {
+  canvas = createCanvas(1520, windowHeight);
+  // canvas.position(0,0);
+  // canvas.style('z-index', '-1');
+
+
+  background(0);
+  activationButton = createButton('Shoot.')
+  activationButton.position(190, windowHeight + 190);
+  activationButton.mouseClicked(shootingBullet);
+}
+
+function draw() {
+
+  timeline();
+
+if (shotBullet == true && bulletX <= 1452) {
+
+  updatePositionBullet();
+  noStroke();
+  fill(255,0,0);
+  ellipse(bulletX,400,15,10);
+  createKilled();
+
+}
+}
+
+function timeline() {
+  stroke(255);
+  line(450,400,1452,400);
+}
+
+function shootingBullet() {
+  shotBullet = true;
+}
+
+function updatePositionBullet() {
+  bulletX += bulletVX;
+}
+
+function createKilled() {
+  if (bulletX == 600 || bulletX == 740 || bulletX == 741 || bulletX == 742 || bulletX == 743 || bulletX == 744 || bulletX == 745) {
+    console.log('hit');
+    fill(255, 0, 0, 60);
+    ellipse(bulletX,400,5,100);
+  }
 }
